89d32a39a3fd5d70ed642827d300d960335f5b36
whoname01 <70155241+itsdominus@users.noreply.github.com>
2021-03-08 12:14:19 +0300

    Updated mainBot.py
    Убрал строки лишние.

--- mainBot.py
+++ mainBot.py
@@ -14,40 +14,24 @@ from io import BytesIO
 from getpass import getpass
 
 client = amino.Client()
-
 client.login(email="email", password="password") #вводим пароль и почту от аккаунта бота
-
 sub_client = amino.SubClient(comId='194234106', profile=client.profile) #вместо "id" введите айди сообщества, в котором будет работать чат
-
 print('Bot status: True! Bot was login')
-
 time.sleep(1)
 
 def socketDelay():
-
     j = 0
-
     while True:
-
         if j >= 300: # = 5 min
-
             print("Выполняю обнову сокетов...")
-
             client.socket.close()
-
             client.socket.start()
-
             print("Обновление сокетов выполено.")
-
             j = 0
-
         j += 1
-
         time.sleep(1)
         
-        
-        
-        
+                
 methods = []
 for x in client.callbacks.chat_methods:
 	methods.append(client.callbacks.event(client.callbacks.chat_methods[x].__name__)(on_message))
